49ccd309e7c21e91bb81e383244337e681cd21a0
peng.yuan <peng.yuan@xuncetech.com>
2022-11-14 15:30:51 +0800

    feat:初步提交

---
+++ src/main/java/com/cn/TestAction.java
@@ -0,0 +1,58 @@
+package com.cn;
+
+
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.MessageType;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.pom.Navigatable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestAction extends AnAction {
+    List<String> resultList = new ArrayList<>();
+
+
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+
+        // Using the event, create and show a dialog
+        Project currentProject = event.getProject();
+        StringBuilder message =
+                new StringBuilder(event.getPresentation().getText() + " Selected!");
+        // If an element is selected in the editor, add info about it.
+        Navigatable selectedElement = event.getData(CommonDataKeys.NAVIGATABLE);
+        if (selectedElement != null) {
+            message.append("\nSelected Element: ").append(selectedElement);
+        }
+        String title = event.getPresentation().getDescription();
+
+        resultList.add("1");
+        System.out.println(resultList);
+        Messages.showMessageDialog(
+                currentProject,
+                resultList.toString(),
+                title,
+                Messages.getInformationIcon());
+
+//        ActionManager actionManager = e.getActionManager();
+//        AnAction testFile = actionManager.getAction("TestFile");
+//
+//        NotificationGroup notificationGroup = new NotificationGroup("testid", NotificationDisplayType.BALLOON, false);
+//        /**
+//         * content :  通知内容
+//         * type  ：通知的类型，warning,info,error
+//         */
+//        Notification notification = notificationGroup.createNotification("测试通知", MessageType.INFO);
+//        Notifications.Bus.notify(notification);
+//
+//        Project project = e.getData(PlatformDataKeys.PROJECT);
+//        Messages.showMessageDialog(project, "helloword", "test2", Messages.getInformationIcon());
+    }
+
+}
---
+++ src/main/resources/readme.md
@@ -0,0 +1,32 @@
+gradle插件版本与idea版本的一些对应关系: https://github.com/JetBrains/gradle-intellij-plugin/releases?page=1
+
+build.gradle文件
+```
+plugins {
+    id 'org.jetbrains.intellij' version '1.3.0'
+    id 'java'
+}
+
+intellij {
+    version = '2021.3.3'
+}
+```
+
+buildPlugin编译中文乱码GBK，build.gradle文件下添加如下配置
+```
+
+tasks.withType(JavaCompile) {
+    options.encoding = "UTF-8"
+}
+```
+
+控制台输出乱码参考：
+https://blog.csdn.net/qq_61601793/article/details/125009791
+
+gradle下载地址：https://services.gradle.org/distributions/
+
+When targeting 2020.3 and later only, using Java 11 is required.
+When targeting 2022.2 and later only, using Java 17 is required.
+
+gradle-wrapper.properties文件指定了gradle版本
+distributionUrl=https\://services.gradle.org/distributions/gradle-7.5.1-bin.zip
